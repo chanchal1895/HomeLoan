7697e4f85f4a1b97f6a38e7be5fce481cd6ce499
chanchal1895 <chanchal.bhardwaj9535@gmail.com>
2022-03-04 19:12:50 +0530

    Commit As of 4032022

--- HomeLoanProject/src/main/java/com/lti/homeloan/bean/UserDetails.java
+++ HomeLoanProject/src/main/java/com/lti/homeloan/bean/UserDetails.java
@@ -2,8 +2,31 @@ package com.lti.homeloan.bean;
 
 
 public class UserDetails {
-
-	
+private String firstname;
+	private String lastname;
+	private long mobileNo;
+	private String gender;
+	private String primaryEmail;
+	private String city;
+	private String state;
+	private double loanAmt;
+	private float loanTenure;
+	private String password;
+	private String userRole;
+	private String loanType;
+	private int loanTypeId;
+	private int ur;
+	private String message;
+	private String subject;
+	private String formName;
+	private String appStatus;
+	private String approvedBy;
+	private String loanTypeName;
+	private String Remarks;
+	private double loanEMI;
+	private double interestRate;
+	private double totIntPayable;
+	private double totPayment;
 	public String getFirstname() {
 		return firstname;
 	}
@@ -46,16 +69,16 @@ public class UserDetails {
 	public void setState(String state) {
 		this.state = state;
 	}
-	public int getLoanAmt() {
+	public double getLoanAmt() {
 		return loanAmt;
 	}
-	public void setLoanAmt(int loanAmt) {
+	public void setLoanAmt(double loanAmt) {
 		this.loanAmt = loanAmt;
 	}
-	public int getLoanTenure() {
+	public float getLoanTenure() {
 		return loanTenure;
 	}
-	public void setLoanTenure(int loanTenure) {
+	public void setLoanTenure(float loanTenure) {
 		this.loanTenure = loanTenure;
 	}
 	public String getPassword() {
@@ -76,13 +99,18 @@ public class UserDetails {
 	public void setLoanType(String loanType) {
 		this.loanType = loanType;
 	}
+	public int getLoanTypeId() {
+		return loanTypeId;
+	}
+	public void setLoanTypeId(int loanTypeId) {
+		this.loanTypeId = loanTypeId;
+	}
 	public int getUr() {
 		return ur;
 	}
 	public void setUr(int ur) {
 		this.ur = ur;
 	}
-	
 	public String getMessage() {
 		return message;
 	}
@@ -95,20 +123,108 @@ public class UserDetails {
 	public void setSubject(String subject) {
 		this.subject = subject;
 	}
+	public String getFormName() {
+		return formName;
+	}
+	public void setFormName(String formName) {
+		this.formName = formName;
+	}
+	public String getAppStatus() {
+		return appStatus;
+	}
+	public void setAppStatus(String appStatus) {
+		this.appStatus = appStatus;
+	}
+	public String getApprovedBy() {
+		return approvedBy;
+	}
+	public void setApprovedBy(String approvedBy) {
+		this.approvedBy = approvedBy;
+	}
+	public String getLoanTypeName() {
+		return loanTypeName;
+	}
+	public void setLoanTypeName(String loanTypeName) {
+		this.loanTypeName = loanTypeName;
+	}
+	public String getRemarks() {
+		return Remarks;
+	}
+	public void setRemarks(String remarks) {
+		Remarks = remarks;
+	}
+	public double getLoanEMI() {
+		return loanEMI;
+	}
+	public void setLoanEMI(double loanEMI) {
+		this.loanEMI = loanEMI;
+	}
+	public double getInterestRate() {
+		return interestRate;
+	}
+	public void setInterestRate(double interestRate) {
+		this.interestRate = interestRate;
+	}
+	public double getTotIntPayable() {
+		return totIntPayable;
+	}
+	public void setTotIntPayable(double totIntPayable) {
+		this.totIntPayable = totIntPayable;
+	}
+	public double getTotPayment() {
+		return totPayment;
+	}
+	public void setTotPayment(double totPayment) {
+		this.totPayment = totPayment;
+	}
+	public UserDetails(String firstname, String lastname, long mobileNo, String gender, String primaryEmail,
+			String city, String state, double loanAmt, float loanTenure, String password, String userRole,
+			String loanType, int loanTypeId, int ur, String message, String subject, String formName, String appStatus,
+			String approvedBy, String loanTypeName, String remarks, double loanEMI, double interestRate,
+			double totIntPayable, double totPayment) {
+		super();
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.mobileNo = mobileNo;
+		this.gender = gender;
+		this.primaryEmail = primaryEmail;
+		this.city = city;
+		this.state = state;
+		this.loanAmt = loanAmt;
+		this.loanTenure = loanTenure;
+		this.password = password;
+		this.userRole = userRole;
+		this.loanType = loanType;
+		this.loanTypeId = loanTypeId;
+		this.ur = ur;
+		this.message = message;
+		this.subject = subject;
+		this.formName = formName;
+		this.appStatus = appStatus;
+		this.approvedBy = approvedBy;
+		this.loanTypeName = loanTypeName;
+		Remarks = remarks;
+		this.loanEMI = loanEMI;
+		this.interestRate = interestRate;
+		this.totIntPayable = totIntPayable;
+		this.totPayment = totPayment;
+	}
+	public UserDetails() {
+//		super();
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public String toString() {
+		return "UserDetails [firstname=" + firstname + ", lastname=" + lastname + ", mobileNo=" + mobileNo + ", gender="
+				+ gender + ", primaryEmail=" + primaryEmail + ", city=" + city + ", state=" + state + ", loanAmt="
+				+ loanAmt + ", loanTenure=" + loanTenure + ", password=" + password + ", userRole=" + userRole
+				+ ", loanType=" + loanType + ", loanTypeId=" + loanTypeId + ", ur=" + ur + ", message=" + message
+				+ ", subject=" + subject + ", formName=" + formName + ", appStatus=" + appStatus + ", approvedBy="
+				+ approvedBy + ", loanTypeName=" + loanTypeName + ", Remarks=" + Remarks + ", loanEMI=" + loanEMI
+				+ ", interestRate=" + interestRate + ", totIntPayable=" + totIntPayable + ", totPayment=" + totPayment
+				+ "]";
+	}
 
-	private String firstname;
-	private String lastname;
-	private long mobileNo;
-	private String gender;
-	private String primaryEmail;
-	private String city;
-	private String state;
-	private int loanAmt;
-	private int loanTenure;
-	private String password;
-	private String userRole;
-	private String loanType;
-	private int ur;
-	private String message;
-	private String subject;
+
+	
 }
